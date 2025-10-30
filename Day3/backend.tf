0ab9faa186def9a322663c94c34e535db5eca7d6
Aishwary Gathe <aishwarygathe@gmail.com>
2025-10-30 23:08:59 +0530

    backend file

--- Day3/backend.tf
+++ Day3/backend.tf
@@ -3,5 +3,6 @@ terraform {
     bucket = "team-alpha-bucket"
     key = "prod/terraformstatefiles/terraform.tfstate"
     region = "us-east-1"
+    dynamodb_table = "terraform-lock-table"
       }
 }
